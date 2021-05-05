d38be324db4b575dabcf8edb958ab7bbdec5b5e2
iamhabbeboy <iamhabbeboy@gmail.com>
2021-05-05 19:32:41 +0100

    build: fix tailwindcss on production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
+  mode: "jit",
   purge: ["./pages/**/*.js", "./components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
